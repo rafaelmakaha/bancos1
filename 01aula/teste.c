56ad1903f2c90cf63f6f7a03af69f71284582f95
Rafael Makaha <rafael.makaha@gmail.com>
2019-03-17 14:09:44 -0300

    Starts repository with first class.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ 01aula/teste.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(){
+    // FILE *f = fopen("carlos.txt", "w");
+    // char a[20] = "teste";
+    // fwrite("teste",sizeof(a),1,f);
+    // fclose(f);
+
+    // FILE *f1 = fopen("carlos.txt", "r");
+    // char teste[20];
+    // fread(&teste, sizeof(teste), 1, f1);
+    // fclose(f1);
+    // printf("%s\n", teste);
+
+    char a[3] = "ab";
+    char b[3] = "cd";
+
+    char c[5];
+
+
+    return 0;
+}
