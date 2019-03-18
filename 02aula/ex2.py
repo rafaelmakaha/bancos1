0bc273c9feb7b81cf9d0e5ec143f23719c380445
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2019-03-17 23:02:01 -0300

    Adds Insertion Function.
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>

--- 02aula/ex2.py
+++ 02aula/ex2.py
@@ -8,12 +8,15 @@
 
 # Imports area
 import os
+import json
 import pickle
 
 # Classes area
-class Proprietario:
-    nome = ""
-    cpf = ""
+class Proprietario(object):
+    def __init__(self):
+        self.nome = ""
+        self.cpf = ""
+        self.carros = []
 
 
 # Functions area
@@ -25,19 +28,66 @@ def menu():
     print("0. Sair")
     print("##############")
     
-def inserir(list prop):
-    limpa_tela()
-    print ("oi")
+def inserir_cpf(prop):
+    cpf = int(input("Insira o CPF do proprietário: "))
+    valido = False
+    if len(prop) == 0:
+        valido = True
+    while valido == False:
+        for item in prop:
+            if item.cpf == cpf:
+                valido = False
+            else:
+                valido = True
+        cpf = int(input("CPF já cadastrado, favor inserir o CPF correto: "))
+    
+    return cpf
 
-def exibir(list prop):
+def inserir_carros():
+    carros = []
+    qnt = int(input("Inisra a quantidade de carros do proprietário: "))
+    while qnt < 1:
+        qnt = int(input("Inisra um valor maior do que zero: "))
+    for i in range(qnt):
+        carros.append(str(input("Insira o nome do carro: ")))
+
+    return carros
+
+def inserir(prop):
+    limpa_tela()
+
+    nome = ""
+    cpf = 0
+    carros = []
+
+    nome = str(input("Insira o nome do proprietário: "))
+    cpf = inserir_cpf(prop)
+    carros = inserir_carros()
+
+    teste = Proprietario()
+    teste.nome = nome
+    teste.cpf = cpf
+    teste.qnt = len(carros)
+    teste.carros = carros
+
+    prop.append(teste)
+    teste = json.dumps(teste.__dict__)
+
+    with open("data.json", "a") as f:
+        json.dump(teste, f)
+        f.write("\n")
+
+def exibir(prop):
     limpa_tela()
     print ("oi2")
 
-def verifica_arquivo():
-    if os.path.isfile('./arquivo.txt'):
-        print("há arquivo")
+def verifica_arquivo(prop):
+    if os.path.isfile('./data.json'):
+        # with open("data.json","r") as f:
+        #     for line in f:
+        print("ok")
     else:
-        print("não há arquivo")
+        open("data.json", "w")
 
 def limpa_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -45,6 +95,9 @@ def limpa_tela():
 def main():
     prop = []
     opcao = 1
+    print(len(prop))
+    verifica_arquivo(prop)
+
     while(opcao > 0):
         menu()
         if opcao > 2 or opcao < 0:
@@ -57,5 +110,5 @@ def main():
             exibir(prop)
 
 
-
+# Execution area
 main()
