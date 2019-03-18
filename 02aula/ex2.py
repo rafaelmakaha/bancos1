f01de9d7abe0fc2423dc992151ba59ea2d3a1f95
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2019-03-18 10:33:35 -0300

    Fixes CPF function and removes comments.
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>

--- 02aula/ex2.py
+++ 02aula/ex2.py
@@ -16,6 +16,7 @@ class Proprietario(object):
     def __init__(self):
         self.nome = ""
         self.cpf = ""
+        self.qnt = ""
         self.carros = []
 
 
@@ -31,18 +32,15 @@ def menu():
 def inserir_cpf(prop):
     cpf = int(input("Insira o CPF do proprietário: "))
     valido = False
-    print(prop)
-    print(prop["proprietarios"][0][:])
     if len(prop["proprietarios"]) == 0:
         valido = True
     while valido == False:
         for item in prop["proprietarios"]:
             if item["cpf"] == cpf:
-                print("já existe")
                 valido = False
                 cpf = int(input("CPF já cadastrado, favor inserir o CPF correto: "))
+                break
             else:
-                print("entrei2")
                 valido = True
     
     return cpf
@@ -64,9 +62,9 @@ def inserir(prop):
     cpf = 123
     carros = ['gol']
 
-    # nome = str(input("Insira o nome do proprietário: "))
-    # cpf = inserir_cpf(prop)
-    # carros = inserir_carros()
+    nome = str(input("Insira o nome do proprietário: "))
+    cpf = inserir_cpf(prop)
+    carros = inserir_carros()
 
     novo = Proprietario()
     novo.nome = nome
@@ -74,14 +72,10 @@ def inserir(prop):
     novo.qnt = len(carros)
     novo.carros = carros
 
-    print(novo)
-    # print(vars(novo))
-    # novo = json.dumps(novo.__dict__)
-    novo = vars(novo)
-    print(novo)
+    # transforma o objeto em dict
+    novo = vars(novo) 
 
     prop["proprietarios"].append(novo)
-    print(prop)
 
     with open("data.json", "w") as f:
         json.dump(prop, f, indent=4)
@@ -97,10 +91,6 @@ def verifica_arquivo(prop):
     if os.path.isfile('./data.json'):
         with open("data.json","r") as f:
             prop = json.load(f)
-            print(prop)
-            # for x in data["proprietarios"]:
-            #     prop["proprietarios"].append(x)
-        # print(prop["proprietarios"][0]["nome"])
 
     else:
         with open("data.json", "w") as f:
@@ -112,18 +102,8 @@ def limpa_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def main():
-    # prop = []
     prop = {}
     prop["proprietarios"] = []
-
-    # prop["proprietarios"].append({
-    #     'nome': 'teste',
-    #     'cpf': 123,
-    #     'carros':['carrinho','teste']
-    # })
-    # with open("data.json", "w") as f:
-    #     json.dump(prop, f, indent=4)
-    #     f.write("\n")
 
     opcao = 1
     prop = verifica_arquivo(prop)
